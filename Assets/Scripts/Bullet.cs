f6f9b2b666fb97fa511a4da3c38b9d3016f72b7a
JY <chy4505@naver.com>
2023-12-13 21:34:51 +0900

    적 체력과 적,배경 사운드 추가

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -9,11 +9,13 @@ public class Bullet : MonoBehaviour
     public GameObject bullet;//총알 에셋 변수 
     public Transform SpawnPoint;//총알 복제 위치 
     public float speed;//총알 속도 변수 
-    float triggerValue;//트리거 입력 변수 설정
+    float triggerValueR;//트리거 입력 변수 설정
+    float triggerValueL;//트리거 입력 변수 설정
     public float spawnTime = 0.5f;//총알 복제 시간
+    public Animator anim;
 
     //오디오 소스 변수
-    //public AudioSource fireAudio;
+    public AudioSource fireAudio;
 
     //입력 속성 변수
     public InputActionProperty inputTriggerActionR;
@@ -22,25 +24,27 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        anim = GetComponent<Animator>();
+        fireAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //변수 값을 설정
-        triggerValue = inputTriggerActionR.action.ReadValue<float>();
-        triggerValue = inputTriggerActionL.action.ReadValue<float>();
+        triggerValueR = inputTriggerActionR.action.ReadValue<float>();
+        triggerValueL = inputTriggerActionL.action.ReadValue<float>();
 
         //총알 시간 증가
         spawnTime += Time.deltaTime;
-        
-        if(triggerValue > 0 && spawnTime > 1f) // 트리거 입력과 spawnTime 변수이 1보다 크면 실행
+
+        if (triggerValueR > 0 && spawnTime > 1f) // 트리거 입력과 spawnTime 변수이 1보다 크면 실행
         {
+            anim.SetTrigger("Fire");
             //bulletObj 게임객체 생성
             GameObject bulletObj = Instantiate(bullet);
             //Pistol 게임객체 오디오 소스 플레이
-            //fireAudio.Play();
+            fireAudio.Play();
             //총알 복제 위치 지정
             bulletObj.transform.position = SpawnPoint.position;
             //총알 객체를 z 방향과 속도로 운동에너지 
@@ -52,12 +56,13 @@ public class Bullet : MonoBehaviour
             spawnTime = 0.5f;
 
         }
-        else if (triggerValue > 0 && spawnTime > 1f) // 트리거 입력과 spawnTime 변수이 1보다 크면 실행
+        else if (triggerValueL > 0 && spawnTime > 1f) // 트리거 입력과 spawnTime 변수이 1보다 크면 실행
         {
+            anim.SetTrigger("Fire");
             //bulletObj 게임객체 생성
             GameObject bulletObj = Instantiate(bullet);
             //Pistol 게임객체 오디오 소스 플레이
-            //fireAudio.Play();
+            fireAudio.Play();
             //총알 복제 위치 지정
             bulletObj.transform.position = SpawnPoint.position;
             //총알 객체를 z 방향과 속도로 운동에너지 
--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -7,16 +7,23 @@ public class EnemyMove : MonoBehaviour
 {
     public Transform target;
     NavMeshAgent agent;
-    private Animator anim;
-    private int hp = 3;
+    public Animator anim;
+    public AudioSource RunAudio;
+    public AudioSource DyingAudio;
+    public AudioSource AttackAudio;
+
+    [HideInInspector]
+    public int hp = 3;
    
-    enum States
+    public enum States
     {
-        idle,
         run,
         attack,
         dying
     }
+
+    States state;
+    bool cancelWait;
 
     // Start is called before the first frame update
     void Start()
@@ -24,8 +31,123 @@ public class EnemyMove : MonoBehaviour
         //NavMeshAgent의 컴포넌트를 가져옴
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
+        state = States.run;
+        StartCoroutine(StateMachine());
+        //AttackAudio = GetComponent<AudioSource>();
+        //RunAudio = GetComponent<AudioSource>();
+        //DyingAudio = GetComponent<AudioSource>();
     }
 
+    IEnumerator StateMachine()
+    {
+        while(state != States.dying)
+        {
+            yield return StartCoroutine(state.ToString());
+            //다음 프레임까지 대기
+        }
+        yield return StartCoroutine(States.dying.ToString());
+    }
+
+    IEnumerator CancelableWait(float t)
+    {
+        cancelWait = false;
+        while (t > 0 && cancelWait == false)
+        {
+            t -= Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    IEnumerator run()
+    {
+        var curAnimStateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+        if(curAnimStateInfo.IsName("run") == false)
+        {
+            anim.Play("run", -1, 0);
+            RunAudio.Play();
+            yield return null;
+        }
+
+        //플레이어까지의 거리가 스탑거리보다 작거나 같으면 공격 애니메이션 실행
+        if(agent.remainingDistance <= agent.stoppingDistance)
+        {
+            ChangeState(States.attack);
+        }
+        
+        else
+        {
+            //run 애니메이션의 한 사이클 동안 대기, 무슨 의미인지 파악필요
+            yield return StartCoroutine(CancelableWait(0.5f));
+        }
+    }
+
+    IEnumerator attack()
+    {
+        var curAnimStateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+        anim.Play("attack", -1, 0);
+        AttackAudio.Play();
+
+        if(agent.remainingDistance > agent.stoppingDistance)
+        {
+            ChangeState(States.run);
+        }
+        else
+        {
+            //attack 애니메이션의 2배 대기, 조절해서 공격 간격 조정가능
+            yield return StartCoroutine(CancelableWait(curAnimStateInfo.length));
+        }
+    }
+
+    IEnumerator dying()
+    {
+        target = null;
+        anim.Play("dying", -1, 0);
+        DyingAudio.Play();
+        yield return null;
+    }
+
+    //상태 변경 함수
+    void ChangeState(States newState)
+    {
+        state = newState;
+    }
+    
+    //플레이어 감지, 피격시 hp감소, 적이 죽는 부분
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            hp -= 1;
+        }
+        if (hp <= 0)
+        {
+            ChangeState(States.dying);
+            Invoke("DestroyEnemy", 5f);
+        }
+    }
+
+    void DestroyEnemy()
+    {
+        Destroy(this.gameObject);
+    }
+
+    void Update()
+    {
+        if (target == null) return;
+
+        //공격 상태일 때 타겟을 바라보게 함
+        if (state == States.attack)
+        {
+            transform.LookAt(target);
+        }
+        agent.SetDestination(target.position);
+    }
+
+    /*
     // Update is called once per frame
     void Update()
     {
@@ -50,4 +172,5 @@ public class EnemyMove : MonoBehaviour
             anim.SetBool("isAttack", false);
         }
     }
+    */
 }
